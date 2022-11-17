3d66d089f89dbc5c71977963495986a031881e40
Goekhan Oezdemir <goekhan.oezdemir90@googlemail.com>
2022-11-17 16:04:17 +0100

    add_some_introduction_text_and_classNames

--- frontend-react/src/container/Header/Header.jsx
+++ frontend-react/src/container/Header/Header.jsx
@@ -1,37 +1,50 @@
-import React from 'react'
-import{motion} from 'framer-motion';
+import React from "react";
+import { motion } from "framer-motion";
 
-import{images}from '../../constants';
+import { images } from "../../constants";
 
-
-import './Header.scss';
-
+import "./Header.scss";
 
 const Header = () => {
   return (
     <div className="app__header app__flex">
       <motion.div
-      whileInView={{x:[-100,0],opacity:[0,1] }}
-      transition={{duration:0.5}}
-      className="app__header-info"
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className="app__header-info"
       >
         <div className="app__header-badge">
-          
           <div className="badge-cmp app__flex">
             <span>👋</span>
-            <div style={{marginLeft:20}}>
+            <div style={{ marginLeft: 20 }}>
               <p className="p-text">Hello, I am </p>
-              <h1 className='head-text'>Gökhan </h1>
+              <h1 className="head-text">Gökhan </h1>
             </div>
           </div>
-
+          <div className="tag-cmp app__flex">
+            <p className="p-text">Web Developer</p>
+            <p className="p-text">Freelancer</p>
+          </div>
         </div>
-
-
-
       </motion.div>
-      </div>
-  )
-}
 
-export default Header
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__header-img"
+      >
+        <img src={images.profile} alt="profile_bg" />
+        <motion.img
+          whileInView={{ opacity: [0, 1] }}
+          transition={{ duration: 0.5, delayChildren: 0.5 }}
+          className="app__header-img"
+        ></motion.img>
+      </motion.div>
+
+      <motion.div></motion.div>
+    </div>
+  );
+};
+
+ 
+export default Header;
